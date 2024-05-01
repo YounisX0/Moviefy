c20ab501e796e7d4844de5719208cb5164604a15
YounisX0 <ahmedyounis2004@gmail.com>
2024-05-01 15:27:29 +0300

    Added admin

--- javascript/script.js
+++ javascript/script.js
@@ -1,7 +1,6 @@
 
 const ball = document.querySelector(".toggle-ball");
-const items = document.querySelectorAll("body,header,.logo,.toggle,.home-text,.nav-link,.copy");
-
+const items = document.querySelectorAll("body,header,.logo,.toggle,.home-text,.nav-link,.copy,.pmg,.elements,.ptable");
 ball.addEventListener("click", () => {
     items.forEach(item => {
         item.classList.toggle("active");
