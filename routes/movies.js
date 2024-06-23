87b3ba785b3f73d8699c5657e5af02c962bbb533
YounisX0 <ahmedyounis2004@gmail.com>
2024-06-23 20:40:50 +0300

    Did the image upload

--- routes/movies.js
+++ routes/movies.js
@@ -1,27 +1,51 @@
 const express = require('express');
 const router = express.Router();
 const Movie = require('../models/movie');
+const multer = require('multer');
 
-router.post('/', async (req, res) => {
+
+const upload = multer({
+  limits: {
+    fileSize: 10 * 1024 * 1024, 
+  },
+});
+
+router.post('/', upload.single('poster'), async (req, res) => {
+  const { title, director, year, genre } = req.body;
+
   try {
-    const movie = new Movie(req.body);
-    await movie.save();
-    res.status(201).send(movie);
+    const newMovie = new Movie({
+      title,
+      director,
+      year,
+      genre,
+    });
+
+    if (req.file) {
+      newMovie.poster = req.file.buffer;
+    }
+
+   
+    await newMovie.save();
+
+    res.status(201).send("New movie has been created");
   } catch (error) {
-    res.status(400).send(error);
+    res.status(500).send(error.message);
   }
 });
+
 
 router.get('/', async (req, res) => {
   try {
     const movies = await Movie.find();
     res.status(200).send(movies);
   } catch (error) {
-    res.status(500).send(error);
+    res.status(500).send(error.message);
   }
 });
+
 router.get('/add', (req, res) => {
-  res.render('addMovie');
+  res.render('addMovie'); 
 });
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -2,10 +2,16 @@ const express = require('express');
 const router = express.Router();
 const User = require('../models/user');
 const bcrypt = require('bcryptjs');
+const multer = require('multer');
+
+const upload = multer({
+  limits: {
+    fileSize: 10 * 1024 * 1024, 
+  },
+});
 
 
-
-router.post('/', async (req, res) => {
+router.post('/', upload.single('avatar'), async (req, res) => {
   const { name, email, password } = req.body;
 
   try {
@@ -13,18 +19,20 @@ router.post('/', async (req, res) => {
     if (existingUser) {
       return res.status(400).json({ message: 'User with this email already exists' });
     }
-
-    const hashedPassword = await bcrypt.hash(password, 10); 
-    const user = new User({
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const newUser = new User({
       name,
       email,
       password: hashedPassword,
     });
+    if (req.file) {
+      newUser.avatar = req.file.buffer;
+    }
 
-    await user.save();
-    res.status(201).send(user);
+    await newUser.save();
+    res.status(201).send("New user has been created");
   } catch (error) {
-    res.status(400).send(error);
+    res.status(500).send(error.message);
   }
 });
 
@@ -34,13 +42,13 @@ router.get('/', async (req, res) => {
     const users = await User.find();
     res.status(200).send(users);
   } catch (error) {
-    res.status(500).send(error);
+    res.status(500).send(error.message);
   }
 });
 
 
 router.get('/add', (req, res) => {
-  res.render('addUser');
+  res.render('addUser'); 
 });
 
 module.exports = router;
